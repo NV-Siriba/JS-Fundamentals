9683c58f7f85fb5e65b1b5ed8b0b5525c033d74f
NV-Siriba <161623658+NV-Siriba@users.noreply.github.com>
2025-07-01 18:00:40 +0300

    Update 5-to_integer.js

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,4 +1,4 @@
 const MyNumber=process.argv.slice(2);
 const number=Number(MyNumber[0]);
 if (isNaN (number)){console.log('Not a number');}
-else{console.log(`My Number: ${MyNumber}`);}
+else{console.log(`My number: ${MyNumber}`);}
