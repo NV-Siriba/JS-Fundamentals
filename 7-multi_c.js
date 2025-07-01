1e58e03165970e9bae1da93deaac07f52d6011ae
NV-Siriba <161623658+NV-Siriba@users.noreply.github.com>
2025-07-01 18:43:58 +0300

    Update 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,4 +1,6 @@
 const x=process.argv.slice(2);
 const numX=Number(x[0]);
-for(let i=0;i<=numX;i++){console.log("C is fun");}
-if(isNaN(numX)){console.log('Missing number of occurrences');}
+for(let i=0;i<=numX;i++)
+{console.log("C is fun");}
+if(isNaN(numX))
+{console.log('Missing number of occurrences');}
