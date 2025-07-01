cbe59081b6df3a3fc626da6f1f9e47ad7c01871d
NV-Siriba <161623658+NV-Siriba@users.noreply.github.com>
2025-07-01 21:12:29 +0300

    Delete 7-multi_c.js

--- 7-multi_c.js
+++
@@ -1,9 +0,0 @@
-const x=process.argv.slice(2);
-const numX=Number(x[0]);
-let i=0
-while(i<=numX){
-    console.log("C is fun")
-    i++;}
-    
-for(isNaN(numX);)
-    {console.log('Missing number of occurrences');}
