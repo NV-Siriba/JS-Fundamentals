1581a2cfa7a174f498d041648f41a11a19e9e8db
NV-Siriba <161623658+NV-Siriba@users.noreply.github.com>
2025-07-01 12:58:32 +0300

    Add files via upload

---
+++ 2-arguments.js
@@ -0,0 +1,10 @@
+const Argu=process.argv.slice(2);
+if (Argu.length==0){console.log("No argument");
+
+}
+ else if (Argu.length==1){console.log("Argument found");
+
+ }
+ else{console.log("Arguments found");
+
+}
