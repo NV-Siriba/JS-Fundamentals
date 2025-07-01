4f5cb70f4c2bc8063f513101618e1b348b98a57e
NV-Siriba <161623658+NV-Siriba@users.noreply.github.com>
2025-07-01 21:12:59 +0300

    Add files via upload

---
+++ 7-multi_c.js
@@ -0,0 +1,9 @@
+const x=process.argv.slice(2);
+const numX=Number(x[0]);
+if(isNaN(numX);)
+    {console.log('Missing number of occurrences');}
+else {let i=0;
+while(i<=numX){
+    console.log("C is fun")
+    i++;}}
+    
